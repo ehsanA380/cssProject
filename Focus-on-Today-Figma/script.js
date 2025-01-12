7ce6ad5aa0e876cdd00194f47aef0535b718e9f6
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2025-01-12 17:04:58 +0530

    alt: script.js file

--- Focus-on-Today-Figma/script.js
+++ Focus-on-Today-Figma/script.js
@@ -1,88 +1,97 @@
 let score = 0;
 let setGoal= false;
 const progress = document.querySelector('.progress');
-const screenWidth = window.innerWidth;
-const input = document.querySelectorAll('input[type="text"]');
-const checkbox= document.querySelectorAll('input[type="checkbox"]')
+const progressMsg = document.querySelector('.progressMsg');
+let screenWidth = window.innerWidth;
+const inputs = document.querySelectorAll('input[type="text"]');
+const checkboxes= document.querySelectorAll('input[type="checkbox"]')
+const warningMsg= document.querySelector('.warningMsg');
+const scoreMsg = document.querySelector('.scoreMsg');
+const inputFields= document.querySelectorAll('.inputFields');
 // const checkbox= document.querySelectorAll('.checkmark')
 
 // console.log(screenWidth);
-input[0].addEventListener('input',()=>{
 
-    if(input[0].value.trim() == "" || input[1].value.trim() == "" || input[2].value.trim() == "" ){
-        setGoal = false;
-        console.log(setGoal);
-    }else{
-        setGoal= true;
-    }
+inputs.forEach(input => {
+   input.addEventListener('input',()=>{
+        if(inputs[0].value.trim() == "" || inputs[1].value.trim() == "" || inputs[2].value.trim() == "" ){
+            setGoal = false;
+            console.log(setGoal);
+        }else{
+            setGoal= true;
+            console.log(setGoal);
+            
+        }
+        warningMsg.innerText='';
+    }) 
 })
-input[1].addEventListener('input',()=>{
 
-    if(input[0].value.trim() == "" || input[1].value.trim() == "" || input[2].value.trim() == "" ){
-        setGoal = false;
-        console.log(setGoal);
-    }else{
-        setGoal= true;
-    }
-})
-input[2].addEventListener('input',()=>{
-
-    if(input[0].value.trim() == "" || input[1].value.trim() == "" || input[2].value.trim() == "" ){
-        setGoal = false;
-        console.log(setGoal);
-    }else{
-        setGoal= true;
-    }
-})
 
 
 // console.log(allGoalSet);
 
 
+checkboxes.forEach(checkbox =>{
+    checkbox.addEventListener('change', (event)=> {
+        if(setGoal){
+            if (event.target.checked) {
+            score++;
+            console.log('Checkbox is checked',score);
+            } else {
+                score--;
+                console.log('Checkbox is unchecked',score);
+            }
+            if (score===0) {
+                scoreMsg.innerHTML='Raise the bar by completing your goals!'
+            }
+            if(score===1){
+                scoreMsg.innerHTML='Well begun is half done!'
+                // console.log('socore 1');
+                
+            }
+            if(score===2){
+                scoreMsg.innerHTML='Just a step away, keep going!'
+            }
+            if(score===3){
+                scoreMsg.innerHTML='Whoa! You just completed all the goals, time for chill :D'
+            }
+            if(screenWidth>700){
+                progress.style.width='191.33333333333334' * score + 'px';
+                
+            }else{
+                progress.style.width='101' * score + 'px';
+            }
+            if(score){
+                progressMsg.innerHTML=+score+'/3'+' completed'
+                progressMsg.style.paddingLeft='24px'
+                
+            }else{
+                progressMsg.innerText='';
+            }
+            // inputFields.forEach(inputField=>{
 
-document.querySelectorAll('input[type="checkbox"]')[0].addEventListener('change', function() {
-    if (this.checked) {
-        score++;
-        console.log('Checkbox is checked',score);
-    } else {
-        score--;
-        console.log('Checkbox is unchecked',score);
-    }
+            //     inputField.classList.add('completed');
+            // })
+        }
+        else{
+            checkbox.checked=false;
+            score=score;
+            console.log(score);
+            warningMsg.innerHTML='Please set all the 3 goals!'
+            warningMsg.style.color='red'; 
+        }
+    });
+})
+
+window.addEventListener('resize',()=>{
+    screenWidth = window.innerWidth;
     if(screenWidth>700){
         progress.style.width='191.33333333333334' * score + 'px';
+        
     }else{
         progress.style.width='101' * score + 'px';
     }
+})
 
-});
-document.querySelectorAll('input[type="checkbox"]')[1].addEventListener('change', function() {
-    if (this.checked) {
-        score++;
-        console.log('Checkbox is checked',score);
-    } else {
-        score--;
-        console.log('Checkbox is unchecked',score);
-    }
-    if(screenWidth>700){
-        progress.style.width='191.33333333333334' * score + 'px';
-    }else{
-        progress.style.width='101' * score + 'px';
-    }
-});
-document.querySelectorAll('input[type="checkbox"]')[2].addEventListener('change', function() {
-    if (this.checked) {
-        score++;
-        console.log('Checkbox is checked',score);
-    } else {
-        score--;
-        console.log('Checkbox is unchecked',score);
-    }
-    if(screenWidth>700){
-        progress.style.width='191.33333333333334' * score + 'px';
-    }else{
-        progress.style.width='101' * score + 'px';
-    }
-
-});
 console.log('hi');
 
